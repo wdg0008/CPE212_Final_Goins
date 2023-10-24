0dd9026832f2914916dac9eab1d048ed009167f6
William <116991920+wdg0008@users.noreply.github.com>
2023-10-24 18:44:43 -0500

    Initial Commit

---
+++ CPE212_Final_Goins/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPE212_Final_Goins
+//
+//  Created by William Goins V on 10/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
